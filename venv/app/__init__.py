44960b2091cd4477730d61a6f856c0528f9b648b
Julia Selyutina <jusereda@gmail.com>
2020-03-15 00:06:38 +0300

    Correction in logging.

--- venv/app/__init__.py
+++ venv/app/__init__.py
@@ -38,8 +38,8 @@ if not app.debug:
             os.mkdir('logs')
         file_handler = RotatingFileHandler('logs/miniblog.log', maxBytes=10240, backupCount=10)
         file_handler.setFormatter(logging.Formatter(
-            '%(asctime)s %(levelname)s: %(message)s [in %(pathnane)s:%(lineno)d]'))
-        file_handler.setLevel(loggin.INFO)
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
 
         app.logger.addHandler(file_handler)
         app.logger.setLevel(logging.INFO)
