be29899109418490f0bfdf01e452ab2bf0b0359a
Julia Selyutina <jusereda@gmail.com>
2020-03-14 23:50:28 +0300

    Added logging.

--- venv/app/__init__.py
+++ venv/app/__init__.py
@@ -1,5 +1,6 @@
+import os
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -32,5 +33,16 @@ if not app.debug:
         )
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/miniblog.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathnane)s:%(lineno)d]'))
+        file_handler.setLevel(loggin.INFO)
+
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Miniblog pet-project')
 
 from app import routes, models, errors
