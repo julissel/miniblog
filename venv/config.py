bf60af459bbc5e01a5048d512e3b7f4172f3ceef
Julia Selyutina <jusereda@gmail.com>
2020-03-23 20:21:43 +0300

    Added config variables for sending message to administrator.

--- venv/config.py
+++ venv/config.py
@@ -6,10 +6,10 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'try-to-gues-what'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['admin@example.com']
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 1
+    MAIL_USERNAME = 'admin@example.com' # input real email
+    MAIL_PASSWORD = 'admin_pass' # input real password
+    ADMINS = ['admin@example.com'] # input real email
     POSTS_PER_PAGE = 3
