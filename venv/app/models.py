0560585ba80f3def4493a01bc348f925513b3eca
Julia Selyutina <jusereda@gmail.com>
2020-03-24 00:42:30 +0300

    Added thread for async-sending reset-password message to user.

--- venv/app/models.py
+++ venv/app/models.py
@@ -64,7 +64,8 @@ class User(UserMixin, db.Model):
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECTER_KEY'], algorithms=['HS256'])['reset_password']
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
