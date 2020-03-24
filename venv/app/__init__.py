999a00b4cdcfc44b5dc36bc26eba749e3eab4ae6
Julia Selyutina <jusereda@gmail.com>
2020-03-24 22:39:12 +0300

    Added bootstrap.

--- venv/app/__init__.py
+++ venv/app/__init__.py
@@ -5,8 +5,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from config import Config
+from flask_bootstrap import Bootstrap
 from flask_mail import Mail
+from config import Config
 
 
 app = Flask(__name__)
@@ -16,6 +17,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
 
 if not app.debug:
