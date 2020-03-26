e09de28d6ed169b5dee1b8de4601967412256c5b
Julia Selyutina <jusereda@gmail.com>
2020-03-26 20:22:39 +0300

    Added flask-moment in templates.

--- venv/app/__init__.py
+++ venv/app/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from flask_mail import Mail
 from config import Config
 
@@ -18,6 +19,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 if not app.debug:
